c72650748999a2736e816ce76056d201bb4c7bdb
John Colella <colella.john@gmail.com>
2016-10-26 10:41:10 -0400

    add new access token for google analytics

--- app/utils/googleRequestsHelper.js
+++ app/utils/googleRequestsHelper.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 googleRequestsHelper = {
   getAnalytics: function () {
     return axios.request({
-      url: 'https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A119576095&start-date=2016-06-01&end-date=2016-09-01&metrics=ga%3Asessions%2Cga%3Abounces%2Cga%3AavgSessionDuration&dimensions=ga%3AreferralPath%2Cga%3Asource%2Cga%3Amedium&access_token=ya29.CjCIA3fPhCuj_z_Fjjfr9NbihrW0QaC10WY-WOgrdPNK5Qhadzs8-LylGsNBxeBDR2E'
+      url: 'https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A119576095&start-date=2016-06-01&end-date=2016-09-01&metrics=ga%3Asessions%2Cga%3Abounces%2Cga%3AavgSessionDuration&dimensions=ga%3AreferralPath%2Cga%3Asource%2Cga%3Amedium&access_token=ya29.CjCIAy6QmlJs7QTD44y6ZQ92G-LCNJhw-0eHfacqw9jmEZJd3Ojt_ybK2i4KH7Opcrk'
     });
   }
 }
